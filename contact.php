2c12413f9733e7946d81062898803f2795994b81
U Hrushikesh Karthik <143626493+Hrushikesh-karthik@users.noreply.github.com>
2024-07-12 08:38:19 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php include 'navbar.php'; ?>
+<div class="contact">
+    <h1>Contact Us</h1>
+    <p>
+        <h2>General Inquiries</h2>
+        <p>For general inquiries, please contact us at:</p>
+        <p>Email: test@gmail.com</p>
+        <p>Phone: 123-456-7890</p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29436.130079757484!2d-47.37639963626863!3d-22.746214603024736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c89bd0ead80fdf%3A0x848be58b5720c248!2sCol%C3%A9gio%20Brasil%20Canad%C3%A1%20-%20Educa%C3%A7%C3%A3o%20Bil%C3%ADngue%20-%20Americana!5e0!3m2!1sen!2sin!4v1720088318815!5m2!1sen!2sin" width="1200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>College</title>
+    <style>
+        .home{
+            margin-top: 50px;
+            margin-right: 100px;
+            margin-left: 280px;
+        }
+    </style>
+</head>
+<body>
+<?php include 'navbar.php'; ?>
+<div class="home">
+    <img src="https://www.georgiancollege.ca/wp-content/uploads/Georgian-Barrie-campus-circle-Fall-2014.jpg" width="900px" height="500px" alt="College">
+</div>
+</body>
+</html>
